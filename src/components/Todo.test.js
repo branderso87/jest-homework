a63138303da2f78221ae86ea9618d08344cdc7eb
Brandi Anderson <branderso87@gmail.com>
2017-08-09 11:41:02 -0500

    all test passing. delete action test

--- src/components/Todo.test.js
+++ src/components/Todo.test.js
@@ -10,3 +10,13 @@
 //     ).toJSON();
 //     expect(tree).toMatchSnapshot();
 // });
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Todo from './Todo'
+
+test('Todo renders correctly', () => {
+  const tree = renderer.create(
+    <Todo todo = {{text: 'Test', id: 1, done: false}} />
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
